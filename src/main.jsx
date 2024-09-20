0fba9c9eb78710651f2721f12cff196dc2ebada0
thiago <thiagod11@hotmail.com>
2024-09-20 16:15:29 -0300

    up

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Routes from './routes/Routes.jsx';
+import { RouterProvider } from "react-router-dom";
+const router = Routes
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import ErrorPage from "../ErrorPage.jsx";
-import App from '../App.jsx'
+import ErrorPage from "../pages/ErrorPage.jsx";
+import App from '../App.jsx';
 
 
 export default createBrowserRouter([
   {
     path: "/calorias/",
-    element: <App/>,
+    element: <App />,
     errorElement: <ErrorPage />,
   },
 ]);
+
